6c1cc97d8ebf052d27e662129afd619dfcea40ea
musaddique shaikh <musaddiqueshaikh900@gmail.com>
2026-06-21 13:18:03 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
 
-    } catch (err) {
+    } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
         process.exit(1);
     }
